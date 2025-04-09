c4e5d1bc0282847a13c70148a85c38c13ba64275
marygitlab <mary200438@gmail.com>
2025-04-09 09:48:43 +0300

    Added tests and GitHub Actions workflows

--- tests/test_user.py
+++ tests/test_user.py
@@ -26,16 +26,31 @@ def test_get_existed_user():
 
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    pass
+     response = client.get("/api/v1/user", params={'email': "nonexistent@example.com"})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
+
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
-    pass
+    new_user = {"email": "newuser@example.com"}
+    response = client.post("/api/v1/user", json=new_user)
+    assert response.status_code == 201
+    assert response.json() == new_user
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    existing_user = {"email": users[0]['email']}
+    response = client.post("/api/v1/user", json=existing_user)
+    assert response.status_code == 400
+    assert response.json() == {"detail": "Email already exists"}
 
 def test_delete_user():
     '''Удаление пользователя'''
-    pass
+        email_to_delete="deleteuser@example"
+        client.post("/api/v1/user", json={"Email": "email_to_delte"})
+
+        esponse = client.delete("/api/v1/user", params={'email': users[0]['email']})
+        assert response.status_code == 200
+        assert response.json() == users[0]
+
